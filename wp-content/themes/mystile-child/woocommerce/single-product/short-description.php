0099377e0e83ffb7825dd0d1c0b71f11a460e530
richiepierce <richie.pierce@gmail.com>
2014-12-31 10:40:14 -0600

    Product Description Changes
    Made changes within the product description to show SHP and FGDB links
    for vectors....and S3Browser links for Imagery

--- wp-content/themes/mystile-child/woocommerce/single-product/short-description.php
+++ wp-content/themes/mystile-child/woocommerce/single-product/short-description.php
@@ -54,16 +54,17 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 	 echo '<strong>Publisher: </strong>'.$publisher.'<br><br>'; 
 	 echo '<strong>Publication Date: </strong>'.$pubdate.'<br><br>'; 
 	 if($pa_attributes['imagery']){
-	 	if($product->get_attribute('imagery') == 'lidar'){
+	 	if($product_cat == 'Elevation'){
 	 		
-	 		echo '<a href="http://www.geostor.org/S3Browser/Default.aspx?bucketname=geostor-elevation.geostor.org"><img src="'.wp_get_attachment_url(7210).'" height="42" width="42">&nbsp;&nbsp;&nbsp;&nbsp;Click Here Browse The LIDAR Repository</a><br><br>'; 
+	 		echo '<a href="http://www.geostor.org/S3Browser/Default.aspx?bucketname=geostor-elevation.geostor.org&path='.$product->get_attribute('imagery').'"><img src="'.wp_get_attachment_url(7210).'" height="42" width="42">&nbsp;&nbsp;&nbsp;&nbsp;Click Here Browse The LIDAR Repository</a><br><br>'; 
 	 		//echo '<a href="http://geostor-vectors.geostor.org/'.$product_cat.'/'.$product->get_sku().'.zip"><img src="'.wp_get_attachment_url(210).'" height="42" width="42">&nbsp;&nbsp;&nbsp;&nbsp;Download the Statewide ZIP file</a><br><br>'; 
 	 	}else{
-	 		echo '<a href="http://www.geostor.org/S3Browser/Default.aspx?bucketname=geostor-imagery.geostor.org"><img src="'.wp_get_attachment_url(7210).'" height="42" width="42">&nbsp;&nbsp;&nbsp;&nbsp;Click Here Browse the Imagery Repository</a><br><br>'; 
+	 		echo '<a href="http://www.geostor.org/S3Browser/Default.aspx?bucketname=geostor-imagery.geostor.org&path='.$product->get_attribute('imagery').'"><img src="'.wp_get_attachment_url(7210).'" height="42" width="42">&nbsp;&nbsp;&nbsp;&nbsp;Click Here Browse the Imagery Repository</a><br><br>'; 
 	 	
 	 	}
 	 }else{
-	 	echo '<a href="http://geostor-vectors.geostor.org/'.$product_cat.'/'.$product->get_sku().'.zip"><img src="'.wp_get_attachment_url(210).'" height="42" width="42">&nbsp;&nbsp;&nbsp;&nbsp;Download the Statewide ZIP file</a><br><br>'; 
+	 	echo '<a href="http://geostor-vectors.geostor.org/'.$product_cat.'/SHP/'.$product->get_sku().'.zip"><img src="'.wp_get_attachment_url(210).'" height="42" width="42">&nbsp;&nbsp;&nbsp;&nbsp;Statewide ZIP file (Shapefile - UTM Zone 15N)</a><br><br>'; 
+	 	echo '<a href="http://geostor-vectors.geostor.org/'.$product_cat.'/FGDB/'.$product->get_sku().'.zip"><img src="'.wp_get_attachment_url(210).'" height="42" width="42">&nbsp;&nbsp;&nbsp;&nbsp;Statewide ZIP file (FGDB - UTM Zone 15N)</a><br><br>';
 	 	// GEOSTOR uncomment for WFS url
 	 	//echo '<a href="http://www.geostor.arkansas.gov/ArcGIS/rest/services/FEATURE_SERVICES/'.$product->get_sku().'/MapServer/0"><img src="'.wp_get_attachment_url(211).'" height="42" width="60">&nbsp;&nbsp;&nbsp;&nbsp;Connect To Web Feature Services</a><br><br>'; /* apply_filters( 'woocommerce_short_description', $post->post_excerpt ) */ 
 	 }
